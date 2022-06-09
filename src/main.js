973e1bef28e9ef3a70333638741bd43a7bea6820
unknown <ernatusjessy@gmail.com>
2022-06-09 18:20:50 +0200

    fix icon problem

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import axios from "axios";
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 const base = axios.create({
   baseURL: "https://hangover.timotheedurand.fr/api/", 
 });
