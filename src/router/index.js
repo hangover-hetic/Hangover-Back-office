5e47a4218a434c57545ca6218d0f4699ba84b710
unknown <ernatusjessy@gmail.com>
2022-06-13 23:11:51 +0200

    add login form

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import FestivalView from '../views/ModifFestivals.vue'
 import CreateFestivals from '../views/CreateFestivals'
+import Connexion from '../views/ConnexionView'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/createfestivals',
     name: 'createfestivals',
     component: CreateFestivals
+  },
+  {
+    path: '/connexion',
+    name: 'connexion',
+    component: Connexion
   },
   {
     path: '/about',
---
+++ src/views/ConnexionView.vue
@@ -0,0 +1,66 @@
+<script>
+
+</script>
+
+<template>
+    <div class="login">
+        <form>
+            <div class="form">
+                <input
+                    class="form-1"
+                    id="email"
+                    type="text"
+                    placeholder="Email"
+                />
+                <input
+                    class="form-2"
+                    id="password"
+                    type="password"
+                    placeholder="mot de passe"
+                />
+                <input
+                    class="login"
+                    id="password"
+                    type="submit"
+                    value="Login"
+                />
+                <input
+                    class="registration"
+                    id="registration"
+                    type="submit"
+                    value="Registration"
+                />
+            </div>
+        </form>
+    </div>
+</template>
+
+<style scoped lang="scss">
+   .login {
+        width: 90%;
+        margin: auto;
+   }
+
+   .form {
+       display: flex;
+       flex-direction: column;
+       position: fixed;
+       margin: auto;
+       top: 50%;
+       left: 50%;
+
+       input {
+           width: 100%;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    margin-bottom: 30px;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+    background: transparent;
+       };
+       
+   }
+ 
+</style>
